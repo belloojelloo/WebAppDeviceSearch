e83eb1401a3129f61153c83de8dbc68d2c825748
Belal Ramadan <belalramadan2005@gmail.com>
2025-09-04 22:55:34 +0300

    changes yet again

--- app.py
+++ app.py
@@ -210,5 +210,5 @@ def perform_search(search_id, part_number, websites):
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))  # use Render’s PORT or fallback to 5000 locally
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
 
